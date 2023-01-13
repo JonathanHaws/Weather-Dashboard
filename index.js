8a9588eff42f2c1ba9d778c7d248f80150ae63be
JonathanHaws <jonathanmhaws1@gmail.com>
2023-01-13 12:25:24 -0700

    combined functions into one get weather for city function

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 
-var apikey = "b02be164d047cfbed86694527d1d3a92";
-
 function getWeatherFor(city){
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=40.76&lon=-111.89&appid=" + apikey)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+    // first fetch is for converting city to geo cordinates (lon, lat)
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=b02be164d047cfbed86694527d1d3a92")
+    .then((cities) => cities.json())
+    .then(function(cities){ // cities is an ordered array with 0 being the best guess for the user requested city
+        fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + cities[0].lat + "&lon=" + cities[0].lon + "&appid=b02be164d047cfbed86694527d1d3a92")
+        .then((forecast) => forecast.json())
+        .then(function(forecast){ 
+            console.log(forecast);
+        })
+    })
 }
 
-getWeatherFor();
-  
+getWeatherFor("Salt Lake City");
 
-function getLatAndLonFor(city){
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + apikey)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-}
 
-getLatAndLonFor("Provo");
+
