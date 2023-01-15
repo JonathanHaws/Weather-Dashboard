385b65c2043cd7911c1bb71aa43cde41efc32ec4
JonathanHaws <jonathanmhaws1@gmail.com>
2023-01-15 14:25:47 -0700

    local storage to store search history. History is dynamically displayed with buttons and buttons can be pressed to load that city

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 
-function getOpenWeatherFor(city,root,apikey){
+function getWeatherFor(city,root,apikey){
+    
+    root.empty(); // clear div then dynamically generate new HTML below 
+
     // first fetch is for converting city to geo cordinates (lon, lat), so cords can be used in the next 2 fetches
     fetch("https://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid="+apikey).then((cities)=>cities.json()).then(function(cities){ 
     // second fetch retrives current weather
     fetch("https://api.openweathermap.org/data/2.5/weather?lat="+cities[0].lat+"&lon="+cities[0].lon+"&units=imperial&appid="+apikey).then((weather)=>weather.json()).then(function(weather){     
     // third fetch retrives forecast data in 3 hour increments for next 5 days
     fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+cities[0].lat +"&lon="+cities[0].lon+"&units=imperial&appid="+apikey).then((forecast)=>forecast.json()) .then(function(forecast){ 
-            
-            console.log(weather); // data retrived from fetch
-            console.log(forecast);  
 
-            root.empty(); // clear div then dynamically generate new HTML below 
+            //console.log(weather); // data retrived from fetch
+            //console.log(forecast);  
+               
+            // search history
+            var history = JSON.parse(localStorage.getItem("history"));
+            if (history == null) { history = { searches:[] }; }
+            history.searches.splice(0,0,weather.name);
+            if (history.searches.length > 5){ history.searches.length = 5;}
+            localStorage.setItem("history",JSON.stringify(history));
+            //console.log ( history);
+            var searches = $("<section>").appendTo(root); 
+            searches.css({"border-radius":"6px 6px 0px 0px"});
+            for(var i=0;i<history.searches.length;i++){
+                var buttontext = history.searches[i];   
+                $("<button>").html(history.searches[i]).appendTo(searches).on("click",function(){ 
+                    getWeatherFor($(this).text(),root,apikey)
+                });
+            }
+
 
             // current weather
             var cityname = $("<section>").appendTo(root); 
-            cityname.css({"border-radius":"6px 6px 0px 0px","background-color":"var(--color1)"});
+            cityname.css({"border-radius":"0px","background-color":"var(--color1)"});
             $("<h2>").text(weather.name).css({color:"var(--color2)"}).appendTo(cityname); 
             var current = $("<section>",{style: "border-radius:0px"}).appendTo(root);  
             $("<img>").attr("src","http://openweathermap.org/img/wn/"+weather.weather[0].icon+"@2x.png").appendTo(current); 
-            $("<h3>").text(new Date().toLocaleDateString()).appendTo(current); 
+            $("<h3>").text("Date: " + new Date().toLocaleDateString()).appendTo(current); 
             $("<h3>").text("Temprature: " + weather.main.temp + "°").appendTo(current); 
             $("<h3>").text("Wind Speed: " + weather.wind.speed + " MPH").appendTo(current); 
             $("<h3>").text("Humidity: " + weather.main.humidity + "%").appendTo(current);
@@ -44,10 +62,12 @@ function getOpenWeatherFor(city,root,apikey){
     })})}) // fetches are nested so code doesn't excecute until they are all complete
 }
 
-getOpenWeatherFor("Salt Lake City",$("#weather"),"b02be164d047cfbed86694527d1d3a92");
-
+// load last city in local storage or load default city
+var defaultcity = JSON.parse(localStorage.getItem("history"))
+if (defaultcity==null) { defaultcity = "Salt Lake City"; } else { defaultcity = defaultcity.searches[0];}
+getWeatherFor(defaultcity,$("#dynamic"),"b02be164d047cfbed86694527d1d3a92");
 
 $("button").on("click",function() {
-    getOpenWeatherFor($("input").val(),$("#weather"),"b02be164d047cfbed86694527d1d3a92");
+    getWeatherFor($("input").val(),$("#dynamic"),"b02be164d047cfbed86694527d1d3a92");
     console.log($("input").val());
   });
